126f32623f8bccef2e80398a33fd0e7dd83fb331
Deepankar Bajpeyi <dbajpeyi@gmail.com>
2016-02-16 14:01:49 +0530

    init commit

---
+++ service_area_api/provider/models.py
@@ -0,0 +1,30 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from django.conf import settings
+import uuid
+
+
+
+class Provider(models.Model):
+
+    ext_id = models.UUIDField(primary_key=True, default= uuid.uuid4, editable=False)
+    name = models.CharField(max_length=128)
+    email = models.EmailField(max_length=64)
+    phone = models.CharField(max_length=20)
+    language=models.CharField(max_length=3, choices=settings.LANGUAGES) 
+    currency = models.ForeignKey('Currency')
+
+
+
+class Currency(models.Model):
+
+    name = models.CharField(max_length=20)
+    code = models.CharField(max_length=3)
+    html_code = models.CharField(max_length=10)
+
+
+
+    def __unicode__(self):
+        return self.name
+
---
+++ service_area_api/settings/settings.py
@@ -0,0 +1,17 @@
+from common import *
+from languages import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'moziodb',
+        'USER': 'deepankar',
+        'PASSWORD': 'gomozio',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
+
+
+
+
