3916282122dd98fb469573c122bf6ef1758ff1af
Deepankar Bajpeyi <dbajpeyi@gmail.com>
2016-02-17 00:01:30 +0530

    add an admin to see everything works

--- service_area_api/provider/admin.py
+++ service_area_api/provider/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.contrib.gis import admin
 
+from provider.models import *
 # Register your models here.
+
+class ProviderAdmin(admin.ModelAdmin):
+    pass
+
+class ServiceAreaAdmin(admin.GeoModelAdmin):
+    pass
+
+class CurrencyAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Provider, ProviderAdmin)
+admin.site.register(ServiceArea, ServiceAreaAdmin)
+admin.site.register(Currency, CurrencyAdmin)
+
