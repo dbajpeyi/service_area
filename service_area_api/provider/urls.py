0ff7175ffa0715a8974db3167250897b06499135
Deepankar Bajpeyi <dbajpeyi@gmail.com>
2016-02-17 00:23:25 +0530

    adding viewset instead to make life easier

--- service_area_api/provider/urls.py
+++ service_area_api/provider/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 from . import views
 from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.routers import DefaultRouter
 
-urlpatterns = [
-    url(r'^providers$', views.ProviderListView.as_view()),
-]
+router = DefaultRouter()
+router.register(r'providers', views.ProviderViewSet)
+urlpatterns = router.urls
+
 
--- service_area_api/provider/views.py
+++ service_area_api/provider/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from .serializers import *
 from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework import viewsets
 
-class ProviderListView(ListAPIView):
+class ProviderViewSet(viewsets.ModelViewSet):
+
     serializer_class = ProviderSerializer
     queryset = Provider.objects.all()
 
